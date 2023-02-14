1ee14562d3f08c3f50b442fa95ac3988e5086aac
Diseyi <filomiinaa@gmail.com>
2023-02-14 09:07:02 +0100

    fix: change about and gui link

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,15 +8,24 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
+        Hi, my name is <HighlightSpan>Diseyi</HighlightSpan>!
       </p>
       <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
+        I'm <HighlightAlt>a Software Engineer</HighlightAlt> with a focus on
+        <HighlightAlt> Front-End Development</HighlightAlt>
       </p>
       <p>
-        I am passionate about writing codes and <br />
+        As a Software Engineer with a background in Chemical Science, <br />
+        I have a unique perspective and set of skills that make me well-suited to the challenges of software engineering. <br />  <br />
+        My strong foundation in problem-solving and analytical thinking, developed through my studies in chemical science, <br />
+        allows me to approach complex technical challenges with confidence and creativity. <br />
         developing web applications to solve real-life challenges.
+      </p>
+      <br />
+      <p className="">
+      In my free time, I write Poems which are focused on helping people build healthy self-identity.  <br />  <br />
+      My creativity and passion for helping others make me a well-rounded and valuable member of my team,  <br />
+      and my unique perspective can help drive innovation and develop new solutions to complex technical challenges.
       </p>
     </AboutWrapper>
   );
